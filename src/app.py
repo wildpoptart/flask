def2624b6d3af03b52cc7123df2bac076240c5f6
wildpoptart <porcshe33@gmail.com>
2022-10-23 23:23:56 -0400

    starting flask version of django project

---
+++ src/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+import sqlite3 as sql
+app = Flask(__name__)
+
+#conn = sql.connect('database.db')
+#conn.execute('CREATE TABLE students (name TEXT, addr TEXT)')
+#conn.close()
+
+@app.route('/')
+def home():
+   return render_template('student.html')
+
+@app.route('/enternew')
+def new_student():
+   return render_template('student.html')
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+   if request.method == 'POST':
+      try:
+         nm = request.form['nm']
+         addr = request.form['jobID']
+
+         
+         with sql.connect("database.db") as con:
+            cur = con.cursor()
+            
+            cur.execute("INSERT INTO students (name,addr) VALUES (?,?)",(nm,addr) )
+            
+            con.commit()
+            msg = "Record successfully added"
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+      
+      finally:
+         return render_template("result.html",msg = msg)
+         con.close()
+
+@app.route('/list')
+def list():
+   con = sql.connect("database.db")
+   con.row_factory = sql.Row
+   
+   cur = con.cursor()
+   cur.execute("select * from students")
+   
+   rows = cur.fetchall();
+   return render_template("list.html",rows = rows)
+
+if __name__ == '__main__':
+    app.run(debug = True)
